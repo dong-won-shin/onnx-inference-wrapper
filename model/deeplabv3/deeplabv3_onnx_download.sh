30e1d363a9f8049558623d5bd19a8607c52e53cf
Dong-Won Shin <dongwon.shin@stradvision.com>
2025-07-03 22:23:45 +0900

    Add download scripts for DeepLabV3 and FlowNets ONNX models
    - Created deeplabv3_onnx_download.sh to download the DeepLabV3 ONNX model.
    - Created flownets_onnx_download.sh to download the FlowNets ONNX model.
    - Both scripts utilize curl for downloading the respective model files from Dropbox.

---
+++ model/deeplabv3/deeplabv3_onnx_download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L -o deeplabv3_sim.onnx https://www.dropbox.com/scl/fi/32nlknozsj6d8vdgrtcth/deeplab_sim.onnx?rlkey=riqzwcszskvqfvwne5x2hp3gd&st=t07x97eq&dl=0
---
+++ model/flownets/flownets_onnx_download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L -o flownets_sim.onnx https://www.dropbox.com/scl/fi/5td5skop0z6um9j08xh9c/flownets.onnx?rlkey=eiga2v1hbjk9r5x4b92fica7j&st=5k0fwih2&dl=0
